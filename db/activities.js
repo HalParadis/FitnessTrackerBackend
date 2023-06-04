372a0408e2098269f9383bfa10a0eecf10924832
Kimberly Phan <kimberly.t.phan01@sjsu.edu>
2023-06-04 01:21:04 -0700

    finished db/activities.js & tested activities

--- db/activities.js
+++ db/activities.js
@@ -19,20 +19,58 @@ async function getAllActivities() {
   const {rows: activities} = await client.query(`
     SELECT * FROM activities;
   `);
+
   return activities;
 }
 
-async function getActivityById(id) {}
+async function getActivityById(id) {
+  const { rows: [ activity ] } = await client.query(`
+    SELECT *
+    FROM activities
+    WHERE id=$1;
+  `, [id]);
 
-async function getActivityByName(name) {}
+  return activity;
+}
+
+async function getActivityByName(name) {
+  const { rows: [ activity ] } = await client.query(`
+    SELECT *
+    FROM activities
+    WHERE name=$1;
+  `, [name]);
+
+  return activity;
+}
 
 // used as a helper inside db/routines.js
-async function attachActivitiesToRoutines(routines) {}
+async function attachActivitiesToRoutines(routines) {
+  
+}
 
-async function updateActivity({ id, ...fields }) {
+async function updateActivity( { id, ...fields } ) {
   // don't try to update the id
   // do update the name and description
   // return the updated activity
+
+  //QUESTION - spread on the object fields -meaning -?
+  console.log("here", fields);
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${ key }"=$${ index + 1 }`
+  ).join(', ');
+
+  if (setString.length === 0) {
+    return;
+  }
+
+  const { rows: [ activity ] } = await client.query(`
+    UPDATE activities
+    SET ${ setString }
+    WHERE id=${ id }
+    RETURNING *;
+  `, [...Object.values(fields)]);
+
+  return activity;
 }
 
 module.exports = {
