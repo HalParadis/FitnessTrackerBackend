b69c74c5c28efada56febca4385bd61771c0c723
HalParadis <halparadis@gmail.com>
2023-06-04 00:13:29 -0700

    Fixed bugs. test:watch db command works

--- db/seedData.js
+++ db/seedData.js
@@ -33,33 +33,34 @@ async function createTables() {
     console.log("Starting to build tables...");
     // create all tables, in the correct order
     await client.query(`
-     CREATE TABLE users(
-      id SERIAL PRIMARY KEY,
-      username VARCHAR(255) UNIQUE NOT NULL,
-      password VARCHAR(255) NOT NULL,
-     );
-    ${console.log('made users table')}
-     CREATE TABLE activities(
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(255) UNIQUE NOT NULL,
-      description TEXT NOT NULL,
-     );
+      CREATE TABLE users(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL
+      );
+      
+      CREATE TABLE activities(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) UNIQUE NOT NULL,
+        description TEXT NOT NULL
+      );
 
-     CREATE TABLE routines(
-      id SERIAL PRIMARY KEY,
-      "creatorId" INTEGER REFERENCES users(id),
-      "isPublic" BOOLEAN DEFAULT false,
-      name VARCHAR(255) UNIQUE NOT NULL,
-      goal TEXT NOT NULL
-     );
-     
-     CREATE TABLE routines_activities(
-      id SERIAL PRIMARY KEY,
-      "routineId" UNIQUE INTEGER REFEREENCES routines(id),
-      "activityId" UNIQUE INTEGER REFERENCES activities(id),
-      duration INTEGER,
-      count INTEGER
-     );
+      CREATE TABLE routines(
+        id SERIAL PRIMARY KEY,
+        "creatorId" INTEGER REFERENCES users(id),
+        "isPublic" BOOLEAN DEFAULT false,
+        name VARCHAR(255) UNIQUE NOT NULL,
+        goal TEXT NOT NULL
+      );
+      
+      CREATE TABLE routines_activities(
+        id SERIAL PRIMARY KEY,
+        "routineId" INTEGER REFERENCES routines(id),
+        "activityId" INTEGER REFERENCES activities(id),
+        UNIQUE("routineId", "activityId"),
+        duration INTEGER,
+        count INTEGER
+      );
 
     `);
   } catch (error) {
